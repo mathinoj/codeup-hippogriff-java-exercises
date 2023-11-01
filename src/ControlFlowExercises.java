8fc87b7e3a9dd215917612b5051112c543691464
Matthias Hinojosa <hinojosamatthias@gmail.com>
2023-11-01 14:25:50 -0500

    Refactored first two  while and do while loops

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,21 +1,28 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-        int i = 5;
-        while (i <= 15) {
-            System.out.println("i is " + i);
-            i++;
-        }
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.println("i is " + i);
+//            i++;
+//        }
+
+//        for(int i=5; i<=15; i++){
+//            System.out.println(i);
+//        }
 
 //Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
 //    } while (false);
-//        int doWhile = 1;
+//        int doWhile = 0;
 //        do{
 //            System.out.println(doWhile*2);
 //            doWhile++;
 //        }
 //        while(doWhile*2 <= 100);
 
+        for(int i=0; i<=100; i++){
+            System.out.println(i++);
+        }
 
 
         //Alter your loop to count backwards by 5's from 100 to -10.
@@ -27,12 +34,12 @@ public class ControlFlowExercises {
 //        while (backWards >= -10);
 
 //Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
-        long squareTwo = 2;
-        do{
-            System.out.println(squareTwo);
-            squareTwo*=squareTwo;
-        }
-        while (squareTwo < 100000);
+//        long squareTwo = 2;
+//        do{
+//            System.out.println(squareTwo);
+//            squareTwo*=squareTwo;
+//        }
+//        while (squareTwo < 100000);
 
 
 
