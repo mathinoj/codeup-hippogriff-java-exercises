d32b27d3ed881b8570d94844e5245ac9f184ac29
Matthias Hinojosa <hinojosamatthias@gmail.com>
2023-11-08 21:51:55 -0600

    Completed server name generator

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,24 +1,25 @@
-import java.util.SplittableRandom;
+import org.w3c.dom.ls.LSOutput;
 
 public class ServerNameGenerator {
-//        String[] adjectives = {"jump", "walk", "crawl", "run", "lay", "sleep", "roll", "flop", "stand", "sit"};
-//        String[] nouns = {"store", "woman", "ball", "plane", "restaurant", "baby", "pen", "shoe", "room", "man"};
-//
-//        String adj = pickRandoString(adjectives);
-
     public static void main(String[] args) {
 
-//        int randoAdj = (int) Math.floor(Math.random() * adjectives.length);
-//        String chosenAdj = adjectives[randoAdj];
-//
-//        int randoNoun = (int) Math.floor(Math.random() * nouns.length);
-//        String chosenNoun = nouns[randoNoun];
-//        System.out.println("Here is your server name: ");
-//        System.out.println(chosenAdj + "-" + chosenNoun);
+        String[] adjectives = {"jump", "walk", "crawl", "run", "lay", "sleep", "roll", "flop", "stand", "sit"};
+        String[] nouns = {"store", "woman", "ball", "plane", "restaurant", "baby", "pen", "shoe", "room", "man"};
+
+        String adj = pickRandomString(adjectives);
+        String noun = pickRandomString(nouns);
 
 
-
-
+        System.out.println("Here is your server name: ");
+        System.out.println(adj + "-" + noun);
 
     }
+
+    //Create a method that will return a random element from an array of >>>>>>>>>strings<<<<<<<<<.
+    private static String pickRandomString(String [] strings) {
+                int randoAdj = (int) Math.floor(Math.random() * strings.length -1);
+//        System.out.println(randoAdj);
+        return strings[randoAdj];
+    }
+
 }
