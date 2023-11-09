5278648dd303cc8e909ead6c0157bd0f83ef3936
Matthias Hinojosa <hinojosamatthias@gmail.com>
2023-11-08 21:25:20 -0600

    Completed ArrayExercise

--- src/ArrayExercises.java
+++ src/ArrayExercises.java
@@ -1,56 +1,49 @@
-import javax.swing.*;
-import java.sql.SQLOutput;
 import java.util.Arrays;
 
 public class ArrayExercises {
-//    int[] numbers = {1, 2, 3, 4, 5};
-//    System.out.println(numbers);
-
-
-
+    //PUBLIC STATIC - can be accessed from within the class as well as outside the class.
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5};
 //        System.out.println(Arrays.toString(numbers));
 //        System.out.println(numbers);
 
 
-
-
 //Create an array that holds 3 Person objects.
 //Assign a new instance of the Person class to each element.
-//        Person sy = new Person("sy");
-//        Person jose = new Person("jose");
-//        Person chris = new Person("chris");
-//        Person[] persons = {sy, jose, chris};
-//        STEP 1 - makes space to store 3 references of peeps
         Person[] persons = new Person[3];
+//        System.out.println("purse: " +Arrays.toString(persons));
         persons[0] = new Person("sy");
         persons[1] = new Person("jose");
         persons[2] = new Person("chris");
-
-//        Person jdub = new Person("j-dub");
-//        persons = Person.addPerson(persons, jdub);
-//
-        Person meech = new Person("big meech");
-        persons = Person.addPerson(persons, meech);
-
-        Person x = new Person("x");
-        persons = Person.addPerson(persons, x);
-////        STEP 2
-//        Person[] morePeeps =;
-//
-//                private static Person[] addPerson(Person[] ppl, Person persons){
-//            Person[] morePpl = null;
-//            return morePpl;
-//        }
-
-        for (Person peep : persons){
-            System.out.printf("Name: %s%n", peep.getName());
-        }
+//        System.out.println("purse: " +Arrays.toString(persons));
 
 
+        printPeeps(persons);
 
+        Person[] addOnePerson = addPerson(persons, new Person("j-dub"));
 
-
+        printPeeps(addOnePerson);
     }
+
+    //
+    //PRIVATE STATIC - can be access from within the class only.
+    //
+    //Create a static method named addPerson. It should accept an array of Person objects, as well as a single person object to add to the passed array. It should return an array whose length is 1 greater than the passed array, with the passed person object at the end of the array.
+    private static Person[] addPerson(Person[] currentArrayOfPeeps, Person addOnePerson){
+        Person[] arrayWithNewPersonAdded = Arrays.copyOf(currentArrayOfPeeps, currentArrayOfPeeps.length +1);
+
+        arrayWithNewPersonAdded[arrayWithNewPersonAdded.length-1] = addOnePerson;
+
+        return arrayWithNewPersonAdded;
+    }
+
+    private static void printPeeps(Person[] printPeopleArray){
+        for(Person person : printPeopleArray){
+            System.out.println(person);
+        }
+    }
+
+
+
+
 }
